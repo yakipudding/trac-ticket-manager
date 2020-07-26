a7257afaac0837f788dbdf657083f50151799033
Yakipudding <butterandshoyu@gmail.com>
2020-07-26 23:25:08 +0900

    rm

--- src/settings/dbconfig.tsx
+++
@@ -1,9 +0,0 @@
-export const dbconfig = {
-  user: '***username***',
-  password: '***password***',
-  server: '***server***\\***instance***',
-  database: '***dbname***',
-  options: {
-    enableArithAbort: false,
-  }
-}
